1476fc84fe39f06e4951994aeec7b08b555269ab
Ankur Gupta <guptaankur.gupta0@gmail.com>
2019-01-01 00:42:18 +0530

    Added starter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
+import 'category.dart';
 
-void main() => runApp();
+void main() {
+  runApp(UnitConverterApp());
+}
 
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          child: Category(),
+        ),
+      ),
+    );
+  }
+}
