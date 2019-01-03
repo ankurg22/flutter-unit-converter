49aebcc5d29ea9c01de75386ca1e09ffb0c6acbf
Ankur Gupta <guptaankur.gupta0@gmail.com>
2019-01-03 23:59:26 +0530

    Added route to ConveterScreen

--- lib/category.dart
+++ lib/category.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:unit_converter/converter_screen.dart';
+import 'package:unit_converter/unit.dart';
 
 final rowHeight = 100.0;
 final cornerRadius = BorderRadius.circular(rowHeight / 2);
@@ -7,16 +9,44 @@ class Category extends StatelessWidget {
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
+  final List<Unit> units;
 
   const Category({
     Key key,
     @required this.name,
     @required this.color,
     @required this.iconLocation,
+    @required this.units,
   })  : assert(name != null),
         assert(color != null),
         assert(iconLocation != null),
+        assert(units != null),
         super(key: key);
+
+  void _navigateToConverter(BuildContext context) {
+    if (Navigator.of(context).canPop()) {
+      Navigator.of(context).pop();
+    }
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+              elevation: 1.0,
+              title: Text(
+                name,
+                style: Theme.of(context).textTheme.display1,
+              ),
+              centerTitle: true,
+              backgroundColor: color,
+            ),
+            body: ConverterScreen(
+              name: name,
+              color: color,
+              units: units,
+            ));
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +61,7 @@ class Category extends StatelessWidget {
             highlightColor: color,
             splashColor: color,
             onTap: () {
-              print("I was tapped");
+              _navigateToConverter(context);
             },
             child: Row(
               children: <Widget>[
--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unit_converter/category.dart';
+import 'package:unit_converter/unit.dart';
 
 final _backgroundColor = Colors.green[100];
 
@@ -29,6 +30,17 @@ class CategoryScreen extends StatelessWidget {
     Colors.red,
   ];
 
+  /// Returns a list of mock [Unit]s.
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final categories = <Category>[];
@@ -38,7 +50,8 @@ class CategoryScreen extends StatelessWidget {
         Category(
             name: _categoryNames[i],
             color: _baseColors[i],
-            iconLocation: Icons.cake),
+            iconLocation: Icons.cake,
+            units: _retrieveUnitList(_categoryNames[i])),
       );
     }
 
